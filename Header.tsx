0aab58c08a1574b03ea7090924bdfe310786daa2
z0st <alejandre.bonnin@gmail.com>
2022-08-22 22:24:24 +0200

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,46 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useState } from "react";
+
+import ThemeToggle from "./ThemeToggle";
+
+const Header = () => {
+  const [value, setValue] = useState('');
+  return (
+    <Flex as="header" width="full" align="center">
+      <Heading as="h1" size="md">
+        <Link href="/">DeFiSEC.exposed</Link>
+        <Text textAlign="right" fontWeight="normal" fontSize="xs">
+          by CIROVE
+        </Text>
+      </Heading>
+
+      <Box marginLeft="auto" display="flex">
+        <InputGroup>
+          <Input
+            borderRadius={11}
+            mt={1}
+            mr={5}
+            placeholder="Search..."
+            size="sm"
+            fontWeight="medium"
+            value={value}
+            onChange={(e) => {
+              setValue(e.currentTarget.value);
+            }}
+          />
+        </InputGroup>
+        <ThemeToggle />
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
