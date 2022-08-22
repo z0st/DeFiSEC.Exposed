5e3d24714254a30f68f0f68b968457da46d9ae7b
z0st <alejandre.bonnin@gmail.com>
2022-08-22 22:52:00 +0200

    Delete search.ts

--- src/pages/api/search.ts
+++
@@ -1,9 +0,0 @@
-import protocols_data from "../../protocols_data";
-
-export default (req, res) => {
-  const results = req.query. ?
-    protocols_data.filter((protocol_data => protocol_data.name.toLowerCase().includes(req.query.q)) : []
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ results }))
-}
